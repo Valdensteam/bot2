5add0ab600a6cdc4df28fbe86d2d4b023480a796
Valdensteam <faabs@hotmail.be>
2015-09-19 18:07:50 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Copy this file to config.js before entering your information.
-var config = {
-	username: "",
-	password: "",
-	greetMsg: "I'm a bot that trades items for keys, send me offer and I'll see if I like it!",
-	admin: '76561198047943960', // Steam ID 64-bit integer
-};
-
-module.exports = config;
